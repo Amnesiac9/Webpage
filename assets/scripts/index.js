600b1cea5d1546c29e1549022cb376507bade233
John <johnmoreau19@gmail.com>
2023-11-08 23:17:32 -0800

    first commit

---
+++ assets/scripts/index.js
@@ -0,0 +1,41 @@
+  // Darkmode JS
+
+    // Select the button
+    const btn = document.querySelector(".darkmode-switch");
+    // Select all elements with the class "dark"
+    const darkElements = document.querySelectorAll(".dark");
+    // Select the theme preference from localStorage
+    const currentTheme = localStorage.getItem("theme");
+  
+    // If the current theme in localStorage is "dark"
+    if (currentTheme == "light") {
+    // then remove the dark class from all dark elements
+    darkElements.forEach((el) => {
+        el.classList.remove("dark");
+    });
+    document.getElementById("darkmode-switch").innerText = "OFF";
+    }
+  
+    // Listen for a click on the button 
+    btn.addEventListener("click", function() {
+        // Toggle the .dark class on each click
+        darkElements.forEach((el) => {
+            el.classList.toggle("dark");
+        });
+
+        let theme = "light";
+        // If the body contains the .dark-theme class...
+        if (document.body.classList.contains("dark")) {
+            // ...then let's make the theme dark
+            theme = "dark";
+            // update the button text
+            document.getElementById("darkmode-switch").innerText = "ON";
+        }
+        else {
+            // update the button text
+            document.getElementById("darkmode-switch").innerText = "OFF";
+        }
+
+        // Then save the choice in localStorage
+        localStorage.setItem("theme", theme);
+    });
